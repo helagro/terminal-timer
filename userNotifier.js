3043cccfb5ffd9fdce69c9334fe2a9147561dbd8
Henrik L <helagro@outlook.com>
2023-11-10 07:17:48 +0100

    unspecified

--- userNotifier.js
+++ userNotifier.js
@@ -1,30 +1,38 @@
-const { exec } = require('child_process')
-const readline = require('readline')
+const { exec } = require("child_process")
+const readline = require("readline")
+const sound = require("play-sound")((opts = {}))
 
+const MESSAGE = "Time is up!"
 
-const MESSAGE = 'Time is up!'
-
-function notify(){
+function notify() {
     displayNotification()
     flashTerminal()
+    playSound()
 }
 
-function displayNotification(){
-    exec(`osascript -e 'display notification "${MESSAGE}" with title "Terminal Timer"'`) //Mac only
+function displayNotification() {
+    exec(
+        `osascript -e 'display notification "${MESSAGE}" with title "Terminal Timer"'`
+    ) //Mac only
 }
 
-function flashTerminal(on = false, i = 60){
+function flashTerminal(on = false, i = 60) {
+    if (on) {
+        process.stdout.write(MESSAGE)
 
-    if(on){
-        process.stdout.write(MESSAGE);
-        
-        if(i < 0) return
+        if (i < 0) return
     } else {
-        readline.clearLine(process.stdout, 0);
-        readline.cursorTo(process.stdout, 0);
+        readline.clearLine(process.stdout, 0)
+        readline.cursorTo(process.stdout, 0)
     }
 
     setTimeout(() => flashTerminal(!on, --i), 60)
 }
 
-module.exports = {notify}
+function playSound() {
+    sound.play("funk.mp3", function (err) {
+        if (err) throw err
+    })
+}
+
+module.exports = { notify }
